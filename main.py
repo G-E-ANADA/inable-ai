7775506ba21318a3dbd85d2f74c99eca39e0aecf
cogito1016 <cogito1016@naver.com>
2024-08-18 16:38:27 +0900

    [init] 환경변수세팅 (kan-37)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-
 from api.main import api_router
 from core.config import settings
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 app = FastAPI()
 
@@ -9,5 +11,8 @@ app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
+
+project_root = Path(__file__).resolve().parent
+load_dotenv(os.path.join(project_root, ".env"))
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
