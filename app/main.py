ebc4918c872d3a2cf97620d6d0810d5f18e5a4b6
cogito1016 <cogito1016@naver.com>
2024-08-21 02:06:03 +0900

    [fix] CORS오류픽스 (kan-37)

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
 from app.api.main import api_router
 from app.core.config import settings
 import os
@@ -10,6 +12,14 @@ app = FastAPI()
 app = FastAPI(
     title=settings.PROJECT_NAME,
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:1234"],  # 허용할 도메인 (React 앱이 실행되는 도메인)
+    allow_credentials=True,
+    allow_methods=["*"],  # 허용할 HTTP 메서드
+    allow_headers=["*"],  # 허용할 HTTP 헤더
 )
 
 project_root = Path(__file__).resolve().parent
